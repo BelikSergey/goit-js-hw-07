46b3500951d27564ffcda8383489868b46e94505
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-10-05 21:21:17 +0300

    внес корректировки

---
+++ JS/task-08.js
@@ -0,0 +1,17 @@
+'use strict';
+const controlsButtonsEl = document.querySelector('#controls, input');
+console.log(controlsButtonsEl);
+
+// function onClick() {
+//   const div = document.createElement('div');
+//   div.classList.add('box');
+//   div.style =
+//     'background-color: ' +
+//     '#' +
+//     (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+//   document.getElementById('container').appendChild(div);
+// }
+
+// document.querySelector('button').addEventListener('click', onClick);
+
+// for (let i = 5; i > 0; i--) onClick();
